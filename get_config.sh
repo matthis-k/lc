d6f5d2c9d4ef4aa50072f409f969a805f58999c4
Matthis K01lble <matthis.kaelble@gmail.com>
2020-12-17 22:41:20 +0100

    improved get_config.sh and added plugin settings to included

--- get_config.sh
+++ get_config.sh
@@ -2,41 +2,16 @@
 
 rm -rf home root
 
-echo "getting included folders and files"
-dirs=()
-
-
-for include in $(sed "s#~#$HOME#g;" included); do 
-    if [ -d $include ]; then
-        dirs+=" $include"
-        dirs+=" $(fd --base-directory "$include" -td -a -H)"
-    elif [ -f $include ]; then
-        files+=" $include"
-    fi
+echo "getting included files"
+files=()
+for include in $(sed 's#~#'$HOME'#g;' included); do 
+    [ -d $include ] && files+=" $(fd --base-directory "$include" -tf -a -H)"
+    [ -f $include ] && files+=" $include"; 
 done
 
-echo "copying folders and files"
-for dir in $dirs; do
-    if [[ "$dir" == $HOME* ]]; then
-        new_dir="home${dir#$HOME}"
-    else
-        new_dir="root${dir}"
-    fi
-
-    mkdir -p $new_dir
-    for file in $(fd -tf -d 1 -a -H --base-directory $dir); do
-        cp $file "$new_dir/"
-    done
-done
-
-echo "adding extra files"
+echo "copying files"
 for file in $files; do
-    if [[ "$file" == $HOME* ]]; then
-        file_dest="home${file#$HOME}"
-    else
-        file_dest="root${file}"
-    fi
-    mkdir -p $file_dest
-    rmdir $file_dest
-    cp $file $file_dest
+    [[ "$file" == $HOME* ]] && file_dest="home${file#$HOME}" || file_dest="root${file}"
+    mkdir -p $(dirname $file_dest)
+    cp $file $(dirname $file_dest)
 done
