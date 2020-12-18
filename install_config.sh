d0e14b603e469060d67bf6d577dec4c002b2a6b3
Matthis <matthis.kaelble@gmail.com>
2020-12-18 01:21:58 +0100

    improven scritpt

--- install_config.sh
+++ install_config.sh
@@ -3,35 +3,22 @@
 ./before_config_install.sh
 
 echo "your current config will be lost! confirm? (y/n)"
-read confirm
-if ! [ "$confirm" == "y" ]; then
-    exit 0
-fi
-unset 'confirm'
+read confirm && [[ $confirm == [Yy] ]] || exit 0 || unset 'confirm'
 
-echo "creating all nesseccary folders"
-for dir in $(fd --base-directory home -td -H); do
-    mkdir -p "a$HOME/$dir"
-done
-
-for dir in $(fd --base-directory root -td -H); do
-    sudo mkdir -p "/$dir"
-done
-
-echo "copying files"
+echo "setting up home"
 for file in $(fd -tf -H --base-directory home); do
-    cp "home/$file" "$HOME/$file"
+    mkdir -p $HOME/$(dirname $file)
+    cp home/$file $HOME/$(dirname $file)
 done
+
+echo "setting up root"
 for file in $(fd -tf -H --base-directory root); do
-    sudo cp "root/$file" "/$file"
+    sudo mkdir -p /$(dirname $file)
+    sudo cp root/$file /$(dirname $file)
 done
 
 echo "do you want to run the post configuration script? (y/n)"
-read confirm
-if ! [ "$confirm" == "y" ]; then
-    exit 0
-fi
-unset 'confirm'
+read confirm && [[ $confirm == [Yy] ]] || exit 0 || unset 'confirm'
 
 
 ./after_config_install.sh
